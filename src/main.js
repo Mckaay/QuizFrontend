5ed29a7ec70a883ffdd43b616fa907872a63ea1a
Michal Nycz <michal1nycz@gmail.com>
2025-02-27 10:47:11 +0100

    Fix variable name

--- src/main.js
+++ src/main.js
@@ -7,11 +7,9 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-console.log(import.meta.env.VITE_APP_BASE_URL);
-
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL ?? "http://localhost";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_BASE_URL ?? "http://localhost";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 
