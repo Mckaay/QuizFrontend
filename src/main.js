5475d43b8793ef60ba630e435af13a994ed29211
Michal Nycz <michal1nycz@gmail.com>
2025-02-27 23:58:09 +0100

    Commit...

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  import.meta.env.VITE_BACKEND_BASE_URL ?? "http://localhost";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_BASE_URL ?? "http://localhost";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 
